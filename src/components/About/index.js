53df7ba5a56e55c0c9353604f4cd95590ecbddd2
Marcos <marcosbadillo089@gmail.com>
2021-09-11 09:25:07 -0500

    updated code

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,8 +4,8 @@ import React from 'react';
 function About() {
   return (
     <section className="my-5">
-  <h1 id="about">I attended the Full-Stack Web Development with The University of Texas at Austin coding bootcamp. 
-  Entry-level Software Developer seeking full time opportunities developing full stack technologies and APIs  .</h1>
+  <h1 id="about">I attended the Full-Stack Web Development with The University of Texas at Austin coding bootcamp and received my certificate. 
+  I am an entry-level Software Developer seeking full time opportunities developing full stack technologies and APIs  .</h1>
 </section>
   );
 }
